4d921791ac724d8d894bd1c5b27fffc09bcd4389
unknown <22520878@gm.uit.edu.vn>
2025-09-11 15:36:57 +0700

    Code CRC cho bootloader

--- Core/Src/Bootloader.c
+++ Core/Src/Bootloader.c
@@ -25,25 +25,22 @@ bool BL_VerifyFirmware(FlashPartitionId_t partId) {
     if (!FlashManager_GetPartitionInfo(partId, &info)) {
         return false;
     }
+    current_flags.CRC = receivedCRC;
     uint32_t calculated_crc = BL_CalculateCRC(info.startAddress, info.size);
-    if (partId == PARTITION_APP_MAIN) {
-        return (calculated_crc == current_flags.main_flag) && (current_flags.main_flag != 0xFFFFFFFF);
-    } else if (partId == PARTITION_APP_BACKUP) {
-        return (calculated_crc == current_flags.rb_flag) && (current_flags.rb_flag != 0xFFFFFFFF);
-    }
-    return false;
+    if(calculated_crc = current_flags.CRC)
+    	return true;
+    else
+    	return false;
 }
 
 
 bool BL_CheckForUpdateRequest(void) {
 
 	if(GPIO_ReadPin(BOOT_PIN) == PIN_SET)
-	        return true;  // Nhấn nút → bootloader nhận firmware
+	    return true;  // Nhấn nút → bootloader nhận firmware
 
     if (current_flags.update_firmware == UPDATE_REQUEST) {
-        Bootflag_t temp_flags = current_flags;
-        temp_flags.update_firmware = 0; // Xóa cờ sau khi đã đọc
-        BL_UpdateFlags(&temp_flags);
+    	current_flags.update_firmwar = 0; // Xóa cờ sau khi đã đọc
         return true;
     }
     	return false;
@@ -71,39 +68,6 @@ void BL_JumpToApplication(uint32_t app_address) {
     }
 }
 
-//void BL_ReceiveAndFlashFirmware(void) {
-//    uint8_t rx_data_buffer[256];
-//    uint16_t received_length;
-//    firmware_received_count = 0;
-//
-//    FlashPartitionInfo_t main_info;
-//    FlashManager_GetPartitionInfo(PARTITION_APP_MAIN, &main_info);
-//
-//
-//
-//    FlashManager_ErasePartition(PARTITION_APP_MAIN);
-//
-//    while (1) {
-//        CANTP_MainFunction();
-//        received_length = CANTP_ReadRxData(rx_data_buffer, sizeof(rx_data_buffer));
-//
-//        if (received_length > 0) {
-//            if (FlashManager_WritePartition(PARTITION_APP_MAIN, rx_data_buffer, received_length) == false) {
-//                 break;
-//            }
-//            firmware_received_count += received_length;
-//            if (firmware_received_count >= main_info.size) {
-//                break;
-//            }
-//        }
-//    }
-//
-//    // Sau khi nạp xong, cập nhật CRC và cờ
-//    current_flags.main_flag = BL_CalculateCRC(main_info.startAddress, main_info.size);
-//    current_flags.boot_flag = FLAG_MAIN_APP_VALID;
-//    BL_UpdateFlags(&current_flags);
-//}
-
 void BL_Run(void) {
     CAN_Init(CAN_MODE_LOOPBACK,9600);
     CANTP_Init();
@@ -111,32 +75,55 @@ void BL_Run(void) {
     // Đọc cờ trạng thái từ Flash
     FlashManager_ReadPartition(PARTITION_DATA_FLAGS, (uint8_t*)&current_flags, sizeof(Bootflag_t));
 
+    // Check for update
+    // Nếu có update vào UDS nhận và ghi vào flash theo sequence requestdownload -> transferdata -> transferexit
     if (BL_CheckForUpdateRequest()) {
     	UDS_Init();
     	while(!update_done) {
     		UDS_MainFunction();
     	}
     	// Check CRC ....
+    	if(BL_VerifyFirmware(update_patition)) {
+    		 UDS_SendPositiveResponse(sid, NULL, 0);
+    		 if(update_partition == PARTITION_APP_MAIN)
+    			 current_flags.boot_flag = FLAG_MAIN_APP_VALID;
+    		 else if(update_partition == PARTITION_APP_BACKUP)
+    			 boot_flag = FLAG_BACKUP_APP_VALID;
+    	} else {
+    		 UDS_SendPositiveResponse(sid, NULL, 0);
+    		 current_flags.boot_flag = FLAG_NO_VALID_APP;
+    	}
     }
 
+    // Jump app ....
     if (current_flags.boot_flag == FLAG_MAIN_APP_VALID) {
-        if (BL_VerifyFirmware(PARTITION_APP_MAIN)) {
-            BL_JumpToApplication(PARTITION_APP_MAIN);
-        } else if (BL_VerifyFirmware(PARTITION_APP_BACKUP)) {
-            BL_JumpToApplication(PARTITION_APP_BACKUP);
-            // Cập nhật cờ
-            current_flags.boot_flag = FLAG_BACKUP_APP_VALID;
-            BL_UpdateFlags(&current_flags);
-        }
+    	BL_UpdateFlags(&current_flags);
+    	BL_JumpToApplication(PARTITION_APP_MAIN);
     } else if (current_flags.boot_flag == FLAG_BACKUP_APP_VALID) {
-        if (BL_VerifyFirmware(PARTITION_APP_BACKUP)) {
-            BL_JumpToApplication(PARTITION_APP_BACKUP);
-        } else if (BL_VerifyFirmware(PARTITION_APP_MAIN)) {
-            BL_JumpToApplication(PARTITION_APP_MAIN);
-            // Cập nhật cờ
-            current_flags.boot_flag = FLAG_MAIN_APP_VALID;
-            BL_UpdateFlags(&current_flags);
-        }
+    	BL_UpdateFlags(&current_flags);
+    	BL_JumpToApplication(PARTITION_APP_BACKUP);
     }
+
+
+
+//    if (current_flags.boot_flag == FLAG_MAIN_APP_VALID) {
+//        if (BL_VerifyFirmware(PARTITION_APP_MAIN)) {
+//            BL_JumpToApplication(PARTITION_APP_MAIN);
+//        } else if (BL_VerifyFirmware(PARTITION_APP_BACKUP)) {
+//            BL_JumpToApplication(PARTITION_APP_BACKUP);
+//            // Cập nhật cờ
+//            current_flags.boot_flag = FLAG_BACKUP_APP_VALID;
+//            BL_UpdateFlags(&current_flags);
+//        }
+//    } else if (current_flags.boot_flag == FLAG_BACKUP_APP_VALID) {
+//        if (BL_VerifyFirmware(PARTITION_APP_BACKUP)) {
+//            BL_JumpToApplication(PARTITION_APP_BACKUP);
+//        } else if (BL_VerifyFirmware(PARTITION_APP_MAIN)) {
+//            BL_JumpToApplication(PARTITION_APP_MAIN);
+//            // Cập nhật cờ
+//            current_flags.boot_flag = FLAG_MAIN_APP_VALID;
+//            BL_UpdateFlags(&current_flags);
+//        }
+//    }
     while(1);
 }

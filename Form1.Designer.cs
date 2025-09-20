bbee34e8a877457ad8d9a5d26d611042a20fd1ea
Dang Thanh Tai <dangthanhtai01102004@gmail.com>
2025-09-20 17:29:37 +0700

    winform trong thư mục debug có file .exe

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CanFirmwareUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonUpload = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonSelect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonUpload
+            // 
+            this.ButtonUpload.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonUpload.Location = new System.Drawing.Point(56, 332);
+            this.ButtonUpload.Name = "ButtonUpload";
+            this.ButtonUpload.Size = new System.Drawing.Size(185, 128);
+            this.ButtonUpload.TabIndex = 1;
+            this.ButtonUpload.Text = "Upload\r\n";
+            this.ButtonUpload.UseVisualStyleBackColor = false;
+            this.ButtonUpload.Click += new System.EventHandler(this.ButtonUpload_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtFilePath.Location = new System.Drawing.Point(281, 176);
+            this.txtFilePath.Multiline = true;
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(969, 73);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(281, 342);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(327, 44);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(410, 417);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(38, 96);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "...\r\n\r\n\r\n";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ButtonSelect
+            // 
+            this.ButtonSelect.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ButtonSelect.BackgroundImage = global::CanFirmwareUploader.Properties.Resources.xam;
+            this.ButtonSelect.Location = new System.Drawing.Point(56, 140);
+            this.ButtonSelect.Name = "ButtonSelect";
+            this.ButtonSelect.Size = new System.Drawing.Size(185, 120);
+            this.ButtonSelect.TabIndex = 0;
+            this.ButtonSelect.Text = "Select firmware\r\n\r\n\r\n\r\n";
+            this.ButtonSelect.UseVisualStyleBackColor = false;
+            this.ButtonSelect.Click += new System.EventHandler(this.ButtonSelect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(350, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ESP32 Firmware Uploader";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1367, 593);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.ButtonUpload);
+            this.Controls.Add(this.ButtonSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonSelect;
+        private System.Windows.Forms.Button ButtonUpload;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CanFirmwareUploader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSelect_Click(object sender, EventArgs e)
+        {
+            // Tạo hộp thoại chọn file
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                // Chỉ cho phép chọn file có đuôi .bin
+                ofd.Filter = "BIN files (*.bin)|*.bin";
+
+                // Nếu người dùng chọn file và bấm OK
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    // Gán đường dẫn file vào TextBox
+                    txtFilePath.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private async void ButtonUpload_Click(object sender, EventArgs e)
+        {
+            // 1. Kiểm tra đã chọn file chưa
+            if (string.IsNullOrWhiteSpace(txtFilePath.Text))
+            {
+                MessageBox.Show("Vui lòng chọn file .bin trước khi tải lên!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 2. Địa chỉ IP của ESP32 (bạn thay đúng IP của mình)
+            string url = "http://192.168.4.1/upload"; // Ví dụ IP của ESP32 ở chế độ AP
+
+            try
+            {
+                lblStatus.Text = "Đang tải lên...";
+                progressBar1.Style = ProgressBarStyle.Marquee; // Chạy dạng "loading"
+
+                using (HttpClient client = new HttpClient())
+                using (var fs = new FileStream(txtFilePath.Text, FileMode.Open, FileAccess.Read))
+                using (var content = new MultipartFormDataContent())
+                {
+                    var progressContent = new ProgressStreamContent(fs, 4096, (sent, total) =>
+                    {
+                        int percent = (int)((sent * 100) / total);
+                        progressBar1.Invoke((Action)(() => progressBar1.Value = percent));
+                        lblStatus.Invoke((Action)(() => lblStatus.Text = $"Đang tải lên: {percent}%"));
+                    });
+
+                    content.Add(progressContent, "firmware", Path.GetFileName(txtFilePath.Text));
+
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        lblStatus.Text = "Tải lên thành công!";
+                        MessageBox.Show("Firmware đã được tải lên ESP32 thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Tải lên thất bại!";
+                        MessageBox.Show($"Lỗi: {response.StatusCode}", "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Lỗi khi tải lên!";
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                progressBar1.Style = ProgressBarStyle.Blocks; // Trả về chế độ bình thường
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class ProgressStreamContent : HttpContent
+    {
+        private readonly Stream _stream;
+        private readonly int _bufferSize;
+        private readonly Action<long, long> _progress; // bytesSent, totalBytes
+
+        public ProgressStreamContent(Stream stream, int bufferSize, Action<long, long> progress)
+        {
+            _stream = stream;
+            _bufferSize = bufferSize;
+            _progress = progress;
+        }
+
+        protected override async Task SerializeToStreamAsync(Stream targetStream, TransportContext context)
+        {
+            var buffer = new byte[_bufferSize];
+            long size = _stream.Length;
+            long uploaded = 0;
+            int read;
+
+            while ((read = await _stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                await targetStream.WriteAsync(buffer, 0, read);
+                uploaded += read;
+                _progress?.Invoke(uploaded, size);
+            }
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = _stream.Length;
+            return true;
+        }
+    }
+
+}
